be2070229ae7a2295cd012c8d30be59cc4d430fc
Daniel de Paula e Silva <danielpes91@gmail.com>
2016-11-22 16:38:03 +0100

    CNAM-149 Created MLPPExposuresTransformer
    CNAM-149 Finished first version of MLPPExposuresTransformer
    
    CNAM-149 Added disease filter
    
    CNAM-149 Added test-case
    
    CNAM-149 Added flexible exposure definition
    
    CNAM-149 Fixed some test cases
    
    CNAM-149 Added test case
    
    CNAM-149 Review

--- src/main/scala/fr/polytechnique/cmap/cnam/filtering/ExposuresTransformer.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/filtering/ExposuresTransformer.scala
@@ -7,7 +7,7 @@ trait ExposuresTransformer extends DatasetTransformer[FlatEvent, FlatEvent] {
 
   // Constant definitions. Should be verified before compiling.
   // In the future, we may want to export them to an external file.
-  val periodStart = makeTS(2006, 1, 1)
+  val StudyStart = makeTS(2006, 1, 1)
 
   def transform(input: Dataset[FlatEvent]): Dataset[FlatEvent]
 }
