ecd1f9c707b0fdffce42755e5d6739b822fbfb00
firas <firas.fbs.1989@gmail.com>
2017-08-31 18:30:39 +0200

    change PIO config class structure

--- src/main/scala/fr/polytechnique/cmap/cnam/study/pioglitazone/PioglitazoneConfig.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/study/pioglitazone/PioglitazoneConfig.scala
@@ -1,12 +1,9 @@
 package fr.polytechnique.cmap.cnam.study.pioglitazone
 
-import java.sql.Timestamp
-
 import com.typesafe.config.{Config, ConfigFactory}
 import fr.polytechnique.cmap.cnam.etl.config.CaseClassConfig
 import org.apache.spark.SparkContext
 import org.apache.spark.sql.SQLContext
-import org.joda.time.DateTime
 import fr.polytechnique.cmap.cnam.util.functions.makeTS
 object PioglitazoneConfig {
 
@@ -18,6 +15,45 @@ object PioglitazoneConfig {
     val environment: String = sqlContext.getConf("env", "test")
     ConfigFactory.parseResources("config/pioglitazone/pioglitazone.conf").resolve().getConfig(environment)
   }
+
+  case class StudyParams(
+    ageReferenceDate: java.sql.Timestamp = makeTS(2006, 12, 31, 23, 59, 59),
+    studyStart: java.sql.Timestamp = makeTS(2006, 1, 1),
+    studyEnd: java.sql.Timestamp = makeTS(2009, 12, 31, 23, 59, 59),
+    lastDate: java.sql.Timestamp = makeTS(2009, 12, 31, 23, 59, 59),
+    cancerDefinition: String = "broad",
+    delayed_entry_threshold: Int = 12 /* keep it, maybe remove the previous one and set false when this param is 0*/)
+
+  case class DiagnosesParams(
+    codesMapDP: List[String] = List("C67", "C77", "C78", "C79"),
+    codesMapDR: List[String] = List("C67", "C77", "C78", "C79"),
+    codesMapDA: List[String] = List("C67"),
+    imbDiagnosisCodes: List[String] = List("C67"),
+                            )
+  case class MedicalActParams(
+    dcirMedicalActCodes: List[String] = List(),
+    mcoCIM10MedicalActCodes: List[String] = List(),
+    mcoCCAMMedicalActCodes: List[String] = List())
+
+  case class FiltersParams(
+    filter_never_sick_patients: Boolean = false, // always true
+    filter_lost_patients: Boolean = false,
+    filter_diagnosed_patients: Boolean = true,
+    diagnosed_patients_threshold: Int = 6, // keep it, maybe remove the previous one and set false when this param is 0
+    filter_delayed_entries: Boolean = true)
+
+  case class DrugsParams(
+    min_purchases: Int = 1, // 1 or 2
+    start_delay: Int = 0, // can vary from 0 to 3
+    purchases_window: Int = 0, // always 0
+    only_first: Boolean = false, /* can be always false and handled in python / C++, but not soon*/)
+
+  case class PioParams(
+    drugs: DrugsParams = DrugsParams(),
+    medicalActs: MedicalActParams = MedicalActParams(),
+    diagnoses: DiagnosesParams = DiagnosesParams(),
+    study: StudyParams = StudyParams(),
+    filters: FiltersParams = FiltersParams())
 
   case class PioglitazoneParams(
     dcirMedicalActCodes: List[String] = List(),
