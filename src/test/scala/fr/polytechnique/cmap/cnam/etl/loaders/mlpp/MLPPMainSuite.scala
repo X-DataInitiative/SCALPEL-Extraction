3e288576b05e83499d9da1f5a97e45140b9e5a80
firas <firas.fbs.1989@gmail.com>
2018-01-26 11:12:20 +0100

    CNAM-282 refactor code
    CNAM-282 Refactor code
    
    
    CNAM 282 correct test
    
    
    CNAM 282 add test
    
    
    CNAM-282 Add parameters
    
    
    CNAM-282 new version
    
    supposes exposures already calculated
    CNAM-282 fix some parameters
    
    
    CNAM-282 adding different components of the class
    
    no tests
    CNAM-282 delete spaces
    
    
    CNAM-282 delete spaces
    
    
    CNAM-282 review changes
    
    
    CNAM-282 change label
    
    
    CNAM-282 review changes
    
    
    CNAM-282 review changes
    
    
    CNAM-282 add tests
    
    
    CNAM-282 correct paths and config
    
    
    CNAM-282 review comment

---
+++ src/test/scala/fr/polytechnique/cmap/cnam/etl/loaders/mlpp/MLPPMainSuite.scala
@@ -0,0 +1,73 @@
+package fr.polytechnique.cmap.cnam.etl.loaders.mlpp
+
+import fr.polytechnique.cmap.cnam.SharedContext
+
+
+class MLPPMainSuite extends SharedContext{
+
+  "getEnv" should "return correct default environment" in {
+    // Given
+    val args = Map[String, String]()
+    val expected = MLPPMain.TestEnv
+
+    // When
+    val result = MLPPMain.getEnv(args)
+
+    // Then
+    assert(result == expected)
+  }
+
+  it should "return the Fall environment when specified" in {
+    // Given
+    val args = Map(("env", "fall"))
+    val expected = MLPPMain.FallEnv
+
+    // When
+    val result = MLPPMain.getEnv(args)
+
+    // Then
+    assert(result == expected)
+  }
+
+  it should "return the CMAP environment when specified" in {
+
+    // Given
+    val args = Map(("env", "cmap"))
+    val expected = MLPPMain.CmapEnv
+
+    // When
+    val result = MLPPMain.getEnv(args)
+
+    // Then
+    assert(result == expected)
+  }
+  
+  "run" should "run the whole pipeline with MLPP featuring" in {
+    val sqlCtx = sqlContext
+    import sqlCtx.implicits._
+
+    // Given
+    val expectedFeatures = Seq(
+      // Patient A
+      MLPPFeature("PA", 0, "Mol1", 0, 0, 0, 0,  0, 1.0),
+      MLPPFeature("PA", 0, "Mol1", 0, 2, 0, 2,  0, 1.0),
+      MLPPFeature("PA", 0, "Mol1", 0, 3, 0, 3,  0, 1.0),
+      MLPPFeature("PA", 0, "Mol2", 1, 2, 0, 2,  1, 1.0),
+      MLPPFeature("PA", 0, "Mol3", 2, 3, 0, 3,  2, 1.0),
+      // Patient B
+      MLPPFeature("PB", 1, "Mol1", 0, 0, 0,  7,  0, 1.0),
+      MLPPFeature("PB", 1, "Mol1", 0, 2, 0,  9,  0, 1.0)
+    ).toDF("patientID", "patientIndex", "moleculeName", "moleculeIndex", "bucketIndex", "lagIndex", "rowIndex", "colIndex", "value")
+
+    // When
+    val configPath = "src/test/resources/config/filtering-broad.conf"
+    MLPPMain.run(sqlContext, Map("conf" -> configPath, "env" -> "test", "study" -> "pioglitazone"))
+
+    // Then
+    val mlppSparseFeatures = sqlCtx.read.parquet(MLPPMain.TestEnv.featuringPath + "/parquet/SparseFeatures")
+
+    assertDFs(mlppSparseFeatures, expectedFeatures)
+
+  }
+
+}
