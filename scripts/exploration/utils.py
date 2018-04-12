a6fe970543c84b59349bd5160c623ec52fce4e0f
Youcef Sebiat <youcefsebiat@gmail.com>
2018-04-12 17:25:02 +0200

    CNAM-311 Add percentage over population stats
    CNAM-311 Add population percentages graphs

--- scripts/exploration/utils.py
+++ scripts/exploration/utils.py
@@ -51,3 +51,68 @@ def patch_dates_axes(ax: Axes) -> Axes:
     plt.gcf().autofmt_xdate()
 
     return ax
+
+
+def add_percentage_axe(ax, total, y_limit=100, ):
+    # Make twin axis
+    ax2 = ax.twinx()
+
+    # Switch so count axis is on right, frequency on left
+    ax2.yaxis.tick_left()
+    ax.yaxis.tick_right()
+
+    # Also switch the labels over
+    ax.yaxis.set_label_position('right')
+    ax2.yaxis.set_label_position('left')
+
+    ax2.set_ylabel('Pourcentage sur Population total [%]')
+
+    # Use a LinearLocator to ensure the correct number of ticks
+    ax.yaxis.set_major_locator(ticker.LinearLocator(11))
+
+    # Fix the frequency range to 0-100
+    ax2.set_ylim(0, y_limit)
+    ax.set_ylim(0, (y_limit / 100) * total)
+    ax.yaxis.set_major_formatter(millify)
+    # And use a MultipleLocator to ensure a tick spacing of 10
+    ax2.yaxis.set_major_locator(ticker.MultipleLocator(10))
+
+    ax2.grid(None)
+
+    ax.text(0.15, 0.85, 'Population Total : {:,}'.format(total),
+            verticalalignment='top', horizontalalignment='left',
+            transform=plt.gcf().transFigure,
+            bbox={'alpha': 0.1, 'pad': 10})
+
+    plt.tight_layout()
+
+    
+def add_x_percentage_axe(ax, total, x_limit=100, ):
+    # Make twin axis
+    ax2 = ax.twiny()
+
+    ax2.xaxis.tick_top()
+    ax.xaxis.tick_bottom()
+
+    # Also switch the labels over
+    ax.xaxis.set_label_position('bottom')
+    ax2.xaxis.set_label_position('top')
+
+    ax2.set_xlabel('Pourcentage sur Population total [%]')
+
+    # Use a LinearLocator to ensure the correct number of ticks
+    ax.xaxis.set_major_locator(ticker.LinearLocator(x_limit//5 + 1))
+
+    # Fix the frequency range to 0-100
+    ax2.set_xlim(0, x_limit)
+    ax.set_xlim(0, (x_limit / 100) * total)
+    ax.xaxis.set_major_formatter(millify)
+    # And use a MultipleLocator to ensure a tick spacing of 10
+    ax2.xaxis.set_major_locator(ticker.MultipleLocator(5))
+
+    ax2.grid(None)
+
+    #ax.text(0.15, 0.85, 'Population Total : {:,}'.format(total),
+    #        verticalalignment='top', horizontalalignment='left',
+    #        transform=plt.gcf().transFigure,
+    #        bbox={'alpha': 0.1, 'pad': 10})
