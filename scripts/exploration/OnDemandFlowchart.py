9cdf130a18ecfdd0fe88d52cc135f9336e814843
Vishy26 <39298059+Vishy26@users.noreply.github.com>
2018-07-03 10:50:28 +0200

    Update OnDemandFlowchart.py

--- scripts/exploration/OnDemandFlowchart.py
+++ scripts/exploration/OnDemandFlowchart.py
@@ -6,8 +6,22 @@ import sys
 from exploration.flowchart import on_demand_flowchart
 
 
-# Define a function which takes Metadata_json, Flowchart_json, and Output Filepath as input, and returns the prepared flowcharts using the file from the input filepaths using the "on_demand_flowchart" function
 def getchart(metapath: str, flowpath: str, outpath: str):
+    """
+    Takes the filepath for metadata and flowchart json and builds flowcharts corresponding to them. Outputs Pdfs of the charts.
+    Parameters
+    ----------
+    metapath
+           Filepath to the metadata
+    flowpath
+           Filepath of the Flowchart Json
+    outpath
+           Filepath for the output Pdfs
+
+    Returns :
+    -------
+           The PDFs for the generated graphs, and saves them to the outpath.
+    """
     with open(metapath, 'r') as fp:
         metadata_json = json.load(fp)
     with open(flowpath, 'r') as fp:
