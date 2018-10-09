8e2af3479a591d7dea2e804ef0ab59563ae7e02e
Dian SUN <sun.dian0720@gmail.com>
2018-10-09 16:19:21 +0200

    CNAM-330: on/off for outcome and exposure in the fall

--- src/test/scala/fr/polytechnique/cmap/cnam/study/fall/FallMainSuite.scala
+++ src/test/scala/fr/polytechnique/cmap/cnam/study/fall/FallMainSuite.scala
@@ -1,6 +1,9 @@
 package fr.polytechnique.cmap.cnam.study.fall
 
 import fr.polytechnique.cmap.cnam.SharedContext
+import fr.polytechnique.cmap.cnam.etl.implicits
+import fr.polytechnique.cmap.cnam.etl.sources.Sources
+import fr.polytechnique.cmap.cnam.study.fall.config.FallConfig
 
 class FallMainSuite extends SharedContext {
 
@@ -30,4 +33,47 @@ class FallMainSuite extends SharedContext {
     // Then
     assert(result.isEmpty)
   }
+
+  "computeExposures" should "return meta data" in {
+
+    //Given
+    val fallConfig = FallConfig.load("", "test")
+
+    import implicits.SourceReader
+    val sources = Sources.sanitize(sqlContext.readSources(fallConfig.input))
+    val expectedOutputPaths = List("target/test/output/drug_purchases/data", "target/test/output/extract_patients/data",
+      "target/test/output/filter_patients/data", "target/test/output/exposures/data")
+    val expectedOutputTypes = List("dispensations", "patients", "exposures")
+
+    //When
+    val result = FallMain.computeExposures(sources, fallConfig)
+    val resultOutputPaths = result.map(_.outputPath.getOrElse("")).toList
+    val resultOutputTypes = result.map(_.outputType.toString).toList
+
+    //Then
+    assert(expectedOutputPaths.forall(resultOutputPaths.contains))
+    assert(expectedOutputTypes.forall(resultOutputTypes.contains))
+  }
+
+  "computeOutcomes" should "return meta data" in {
+
+    //Given
+    val fallConfig = FallConfig.load("", "test")
+
+    import implicits.SourceReader
+    val sources = Sources.sanitize(sqlContext.readSources(fallConfig.input))
+    val expectedOutputPaths = List("target/test/output/diagnoses/data", "target/test/output/acts/data",
+      "target/test/output/liberal_acts/data", "target/test/output/fractures/data")
+    val expectedOutputTypes = List("diagnosis", "acts", "outcomes")
+
+    //When
+    val result = FallMain.computeOutcomes(sources, fallConfig)
+    val resultOutputPaths = result.map(_.outputPath.getOrElse("")).toList
+    val resultOutputTypes = result.map(_.outputType.toString).toList
+
+    //Then
+    assert(expectedOutputPaths.forall(resultOutputPaths.contains))
+    assert(expectedOutputTypes.forall(resultOutputTypes.contains))
+  }
+
 }
