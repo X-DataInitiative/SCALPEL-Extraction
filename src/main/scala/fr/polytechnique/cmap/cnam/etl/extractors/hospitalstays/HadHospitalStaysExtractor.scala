0f314cb2777ffe228af6145d1dbc648462b23539
strayMat <matt.dout@gmail.com>
2019-09-23 12:27:30 +0200

    Had hospital stay extractor passing tests

---
+++ src/main/scala/fr/polytechnique/cmap/cnam/etl/extractors/hospitalstays/HadHospitalStaysExtractor.scala
@@ -0,0 +1,30 @@
+package fr.polytechnique.cmap.cnam.etl.extractors.hospitalstays
+
+import java.sql.{Date, Timestamp}
+
+import fr.polytechnique.cmap.cnam.etl.events.{EventBuilder, HospitalStay}
+import fr.polytechnique.cmap.cnam.etl.extractors.had.HadExtractor
+import fr.polytechnique.cmap.cnam.etl.sources.Sources
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, Row}
+
+object HadHospitalStaysExtractor extends HadExtractor[HospitalStay] {
+  override val columnName: String = ColNames.EndDate
+  override val eventBuilder: EventBuilder = HospitalStay
+
+  override def extractEnd(r: Row): Option[Timestamp] = Some(new Timestamp(r.getAs[Date](ColNames.EndDate).getTime))
+
+  override def extractStart(r: Row): Timestamp = new Timestamp(r.getAs[Date](ColNames.StartDate).getTime)
+
+  override def isInStudy(codes: Set[String])(row: Row): Boolean = true
+
+  override def code: Row => String = extractGroupId
+
+  override def getInput(sources: Sources): DataFrame = sources.had.get.select(ColNames.hospitalStayPart.map(col): _*)
+
+  override def extractGroupId(r: Row): String = {
+    r.getAs[String](ColNames.EtaNumEpmsi) + "_" +
+      r.getAs[String](ColNames.RhadNum) + "_" +
+      r.getAs[Int](ColNames.Year).toString
+  }
+}
