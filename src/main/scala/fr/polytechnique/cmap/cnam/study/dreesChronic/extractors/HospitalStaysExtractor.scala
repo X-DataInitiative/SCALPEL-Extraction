02085364868fc60a1e94c1dbc511623473504fc9
vjarry <viktor.jarry@sante.gouv.fr>
2019-09-23 12:36:26 +0200

    SSR HospitalStays extraction
    Correction of the type of event in the HospitalStaysExtractor class

--- src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/extractors/HospitalStaysExtractor.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/extractors/HospitalStaysExtractor.scala
@@ -1,7 +1,6 @@
 package fr.polytechnique.cmap.cnam.study.dreesChronic.extractors
 
-import fr.polytechnique.cmap.cnam.etl.events.{Diagnosis, Event}
-import fr.polytechnique.cmap.cnam.etl.extractors.diagnoses._
+import fr.polytechnique.cmap.cnam.etl.events.{HospitalStay, Event}
 import fr.polytechnique.cmap.cnam.etl.extractors.hospitalstays.{McoHospitalStaysExtractor, SsrHospitalStaysExtractor}
 import fr.polytechnique.cmap.cnam.etl.sources.Sources
 import fr.polytechnique.cmap.cnam.util.functions.unionDatasets
@@ -9,7 +8,7 @@ import org.apache.spark.sql.Dataset
 
 class HospitalStaysExtractor() {
 
-  def extract(sources: Sources): Dataset[Event[Diagnosis]] = {
+  def extract(sources: Sources): Dataset[Event[HospitalStay]] = {
 
     val mcoHospitalStays = McoHospitalStaysExtractor.extract(sources, Set.empty)
     val ssrHospitalStays = SsrHospitalStaysExtractor.extract(sources, Set.empty)
