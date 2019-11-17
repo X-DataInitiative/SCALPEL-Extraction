68af2a603f0c3aa33187e6af6c474c5cb673ab18
strayMat <matt.dout@gmail.com>
2019-11-17 18:27:22 +0100

    Correction of the ngap act extractor from the dcir : - taking into account ngap key letter AND ngap coefficient instead of filtering on prs_nat_ref value and return only ngap coefficient

--- src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/codes/Ams.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/codes/Ams.scala
@@ -2,8 +2,9 @@ package fr.polytechnique.cmap.cnam.study.dreesChronic.codes
 
 import fr.polytechnique.cmap.cnam.etl.extractors.ngapacts.NgapActClassConfig
 
+// prs_nat_ref == 3125
  object Ams extends NgapActClassConfig {
-   val naturePrestation: Int = 3125
+   val ngapKeyLetters: Seq[String] = Seq("AMS")
    val ngapCoefficients: Seq[String] = Seq(
      "9.5"
    )
