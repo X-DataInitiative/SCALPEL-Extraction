aad71a644a505862e5abca1e7fad743e205e2455
strayMat <matt.dout@gmail.com>
2019-11-18 16:16:41 +0100

    uncomment commented tests for Csarr acts and SSR_CE filters

--- src/test/scala/fr/polytechnique/cmap/cnam/etl/sources/data/SsrFiltersSuite.scala
+++ src/test/scala/fr/polytechnique/cmap/cnam/etl/sources/data/SsrFiltersSuite.scala
@@ -38,35 +38,35 @@ class SsrFiltersSuite extends SharedContext {
     assertDFs(result, expected)
   }
 
-//  "filterSsrCeCorruptedHospitalStays" should "remove lines that indicates fictional hospital stays in MCO_CE" in {
-//    val sqlCtx = sqlContext
-//    import sqlCtx.implicits._
-//
-//    // Given
-//    val colNames = List(
-//      SsrCeSource.NIR_RET,
-//      SsrCeSource.NAI_RET,
-//      SsrCeSource.SEX_RET,
-//      SsrCeSource.IAS_RET,
-//      SsrCeSource.ENT_DAT_RET
-//    ).map(col => col.toString)
-//
-//    val input = Seq(
-//      ("0", "0", "0", "0", "0"),
-//      ("1", "1", "1", "1", "1"),
-//      ("1", "0", "0", "0", "0")
-//    ).toDF(colNames: _*)
-//
-//
-//    val expected = Seq(
-//      ("0", "0", "0", "0", "0") //filtered
-//    ).toDF(colNames: _*)
-//
-//    // When
-//    val instance = new SsrFilters(input)
-//    val result = instance.filterSsrCeCorruptedHospitalStays
-//
-//    // Then
-//    assertDFs(result, expected)
-//  }
+  "filterSsrCeCorruptedHospitalStays" should "remove lines that indicates fictional hospital stays in SSR_CE" in {
+    val sqlCtx = sqlContext
+    import sqlCtx.implicits._
+
+    // Given
+    val colNames = List(
+      SsrCeSource.NIR_RET,
+      SsrCeSource.NAI_RET,
+      SsrCeSource.SEX_RET,
+      SsrCeSource.IAS_RET,
+      SsrCeSource.ENT_DAT_RET
+    ).map(col => col.toString)
+
+    val input = Seq(
+      ("0", "0", "0", "0", "0"),
+      ("1", "1", "1", "1", "1"),
+      ("1", "0", "0", "0", "0")
+    ).toDF(colNames: _*)
+
+
+    val expected = Seq(
+      ("0", "0", "0", "0", "0") //filtered
+    ).toDF(colNames: _*)
+
+    // When
+    val instance = new SsrFilters(input)
+    val result = instance.filterSsrCeCorruptedHospitalStays
+
+    // Then
+    assertDFs(result, expected)
+  }
 }
