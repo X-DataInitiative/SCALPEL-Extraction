e635b08310f91a786bccd07c3c892e098abb898c
strayMat <matt.dout@gmail.com>
2019-11-20 17:53:09 +0100

    added scala doc for the SsrCsarrActExtractor

--- src/main/scala/fr/polytechnique/cmap/cnam/etl/extractors/acts/SsrActExtractor.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/etl/extractors/acts/SsrActExtractor.scala
@@ -9,6 +9,18 @@ object SsrCcamActExtractor extends SsrExtractor[MedicalAct] {
   override val eventBuilder: EventBuilder = SsrCCAMAct
 }
 
+/** Extract Csarr codes :
+  *
+  *  The Specific Catalogue of Acts of Rehabilitation and Rehabilitation (CSARR) is intended to
+  *  describe and code the activity of the professionals concerned in follow-up care and
+  *  rehabilitation establishments (SSR). These acts are to be distinguished from CCAM acts which
+  *  are the sole responsibility of the doctor.
+  *
+  *  This terminology is of the form `AAA+111`, eg. *GKQ+139 : Évaluation initiale du langage écrit*
+  *
+  *  The complete terminology can be found here : https://drees.shinyapps.io/dico-snds/?variable=FP_PEC&search=csar&table=T_SSRaa_nnB
+  *  For more details see : https://www.atih.sante.fr/sites/default/files/public/content/3302/csarr_2018.pdf
+  */
 object SsrCsarrActExtractor extends SsrExtractor[MedicalAct] {
   final override val columnName: String = ColNames.CSARR
   override val eventBuilder: EventBuilder = SsrCSARRAct
