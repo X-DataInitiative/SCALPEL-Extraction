32b3ba8481f2087f1338c7f3bb50c8350b601984
strayMat <matt.dout@gmail.com>
2019-12-04 16:39:11 +0100

    - update codes for dreesChronic study following HAS instructions
    - added HadMainTakeOverExtractor Reason to the dreesChronicMain

--- src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/codes/Bronchodilatateurs.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/codes/Bronchodilatateurs.scala
@@ -5,91 +5,42 @@ import fr.polytechnique.cmap.cnam.etl.extractors.drugs.classification.{DrugClass
 object Bronchodilatateurs extends DrugClassConfig {
 
   val name: String = "Bronchodilatateurs"
-  val cip13Codes: Set[String] = Set(
-    "3400931812616",
-    "3400932668274",
-    "3400932698721",
-    "3400932962020",
-    "3400932967513",
-    "3400932990344",
-    "3400933029777",
-    "3400933030148",
-    "3400933123031",
-    "3400933189617",
-    "3400933286170",
-    "3400933457396",
-    "3400933457686",
-    "3400933457808",
-    "3400933459000",
-    "3400933459468",
-    "3400933459697",
-    "3400933536381",
-    "3400933773373",
-    "3400933773434",
-    "3400933919467",
-    "3400933919818",
-    "3400934060335",
-    "3400934060625",
-    "3400934082276",
-    "3400934082337",
-    "3400934438738",
-    "3400934653896",
-    "3400935159052",
-    "3400935320742",
-    "3400935321114",
-    "3400935842664",
-    "3400936351820",
-    "3400936412026",
-    "3400936412194",
-    "3400936566460",
-    "3400936573093",
-    "3400936573154",
-    "3400936573383",
-    "3400936578067",
-    "3400936578418",
-    "3400936579019",
-    "3400936579309",
-    "3400936579538",
-    "3400936579996",
-    "3400936580138",
-    "3400936580367",
-    "3400936580596",
-    "3400936580947",
-    "3400936581319",
-    "3400936581548",
-    "3400936581777",
-    "3400936584389",
-    "3400936715165",
-    "3400936715394",
-    "3400936869202",
-    "3400936951266",
-    "3400936951495",
-    "3400937672689",
-    "3400937672740",
-    "3400937672979",
-    "3400938192032",
-    "3400938940091",
-    "3400938940671",
-    "3400938953688",
-    "3400938954111",
-    "3400939982236",
-    "3400939982526",
-    "3400949129560",
-    "3400949411429",
-    "3400949411658"
-  )
+  val cip13Codes: Set[String] = Set()
 
   /*
   list pharmacological classes
    */
   val adrenergiquesBeta2= new PharmacologicalClassConfig(
     name = "Bronchodilatateurs_Adrenergiques_Beta2",
-    ATCCodes = List("R03AC*")
+    ATCCodes = List(
+      "R03AC02", // Bêta-2 agoniste de courte durée d’action
+      "R03AC03",
+      "R03AL01", // Bêta-2 + Anticholinergique de courte durée d’action
+      "R03AC13", // Beta-2 agonistes de longue durée d’action (LABA)
+      "R03AC18",
+      "R03AC19",
+      "R03AC12",
+      "R03AL04", // Bêta-2 agoniste de longue durée d’action et Anticholinergique de longue durée d’actio
+      "R03AL06",
+      "R03BB54",
+      "R03AL03",
+      "R03AK08", // Beta 2 agoniste de longue durée d’action et corticostéroïde inhalé
+      "R03AK06",
+      "R03AK07",
+      "R03AK10",
+      "R03AL09", // beta 2 agoniste de longue durée d’action + anticholinergique de longue durée d’action + corticostéroïde inhalé
+      "R03AL08"
+    )
   )
 
   val adrenergiquesAnticolinergiques= new PharmacologicalClassConfig(
     name = "Bronchodilatateurs_Adrenergiques_anticolinergiques",
-    ATCCodes = List("R03BB*")
+    ATCCodes = List(
+      "R03BB01", //ATC Anticholinergique de courte durée d’action
+      "R03BB06", // Anticholinergique de longue durée d’action (LAMA)
+      "R03BB04",
+      "R03BB07"
+    )
   )
   val pharmacologicalClasses = List(
     adrenergiquesBeta2,
--- src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/codes/Vaccins.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/study/dreesChronic/codes/Vaccins.scala
@@ -6,15 +6,7 @@ object Vaccins extends DrugClassConfig {
 
   val name: String = "Vaccins"
   // Does it work to put empty cip13 codes ?
-  val cip13Codes: Set[String] = Set(
-    "3400933607852",
-    "3400933652166",
-    "3400935493828",
-    "3400935947383",
-    "3400939886091",
-    "3400939886152",
-    "3400939925745"
-  )
+  val cip13Codes: Set[String] = Set()
 
   /*
   list pharmacological classes
@@ -22,7 +14,11 @@ object Vaccins extends DrugClassConfig {
 
   val vaccinAntiGrippal= new PharmacologicalClassConfig(
     name = "Vaccin_Anti_Grippal",
-    ATCCodes = List("J07BB02")
+    ATCCodes = List(
+      "J07BB01",
+      "J07BB02",
+      "J07BB03"
+    )
   )
 
   val pharmacologicalClasses = List(
