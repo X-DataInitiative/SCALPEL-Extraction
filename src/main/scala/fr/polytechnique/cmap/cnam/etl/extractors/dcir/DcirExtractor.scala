41eecd0997d7503cf9a2d9f78894bc107a60aaf8
vjarry <viktor.jarry@sante.gouv.fr>
2020-02-14 18:30:21 +0100

    DREES-106: Dealing with SSR and MCO CE extractors logic

--- src/main/scala/fr/polytechnique/cmap/cnam/etl/extractors/dcir/DcirExtractor.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/etl/extractors/dcir/DcirExtractor.scala
@@ -4,6 +4,7 @@ package fr.polytechnique.cmap.cnam.etl.extractors.dcir
 
 import java.sql.Timestamp
 import org.apache.commons.codec.binary.Base64
+import scala.util.Try
 import org.apache.spark.sql.functions.col
 import org.apache.spark.sql.{DataFrame, Row}
 import fr.polytechnique.cmap.cnam.etl.events.{AnyEvent, Event, EventBuilder}
@@ -40,7 +41,17 @@ trait DcirExtractor[EventType <: AnyEvent] extends Extractor[EventType] with Dci
     r.getAs[String](ColNames.PatientID)
   }
 
-  def extractStart(r: Row): Timestamp = r.getAs[java.util.Date](ColNames.Date).toTimestamp
+   /** Trying to catch unknown dates (example : IJ = Indemnité Journalière)
+   *
+   * @param r The Row object itself
+   * @return The date of the event or the flux date if it doesn't exist
+   */
+  def extractStart(r: Row): Timestamp = {
+    Try(r.getAs[java.util.Date](ColNames.Date).toTimestamp) recover {
+      case _: NullPointerException => extractFluxDate(r)
+    }
+  }.get
+
 
   def extractFluxDate(r: Row): Timestamp = r.getAs[java.util.Date](ColNames.DcirFluxDate).toTimestamp
 
