8d753c7b1598d2309462e7c7ecc52eba423719d3
vjarry <viktor.jarry@sante.gouv.fr>
2020-02-17 09:58:32 +0100

    DREES-106: Adding documentation on the sources. Adding IR_NAT_REF referentiel.
    Revert "Adding documentation on the sources. Adding IR_NAT_REF referentiel."
    
    Those changes should be in the Sources modification Merge Request for clarity
    
    Adding IR_NAT_REF for Nap extractor

--- src/main/scala/fr/polytechnique/cmap/cnam/etl/sources/Sources.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/etl/sources/Sources.scala
@@ -5,8 +5,8 @@ package fr.polytechnique.cmap.cnam.etl.sources
 import java.sql.Timestamp
 import org.apache.spark.sql.{DataFrame, SQLContext}
 import fr.polytechnique.cmap.cnam.etl.config.study.StudyConfig.InputPaths
-import fr.polytechnique.cmap.cnam.etl.sources.data.{DcirSource, McoCeSource, McoSource, SsrSource, SsrCeSource, HadSource}
-import fr.polytechnique.cmap.cnam.etl.sources.value.{DosagesSource, IrBenSource, IrImbSource, IrPhaSource}
+import fr.polytechnique.cmap.cnam.etl.sources.data.{DcirSource, HadSource, McoCeSource, McoSource, SsrCeSource, SsrSource}
+import fr.polytechnique.cmap.cnam.etl.sources.value.{DosagesSource, IrBenSource, IrImbSource, IrNatSource, IrPhaSource}
 
 case class Sources(
   dcir: Option[DataFrame] = None,
@@ -18,6 +18,7 @@ case class Sources(
   irBen: Option[DataFrame] = None,
   irImb: Option[DataFrame] = None,
   irPha: Option[DataFrame] = None,
+  irNat: Option[DataFrame] = None,
   dosages: Option[DataFrame] = None)
 
 object Sources {
@@ -33,6 +34,7 @@ object Sources {
       irBen = sources.irBen.map(IrBenSource.sanitize),
       irImb = sources.irImb.map(IrImbSource.sanitize),
       irPha = sources.irPha.map(IrPhaSource.sanitize),
+      irNat = sources.irNat.map(IrNatSource.sanitize),
       dosages = sources.dosages.map(DosagesSource.sanitize)
     )
   }
@@ -44,9 +46,11 @@ object Sources {
       ssr = sources.ssr.map(SsrSource.sanitizeDates(_, studyStart, studyEnd)),
       had = sources.had.map(HadSource.sanitizeDates(_, studyStart, studyEnd)),
       mcoCe = sources.mcoCe.map(McoCeSource.sanitizeDates(_, studyStart, studyEnd)),
+      ssrCe = sources.ssrCe.map(SsrCeSource.sanitizeDates(_, studyStart, studyEnd)),
       irBen = sources.irBen,
       irImb = sources.irImb,
       irPha = sources.irPha,
+      irNat = sources.irNat,
       dosages = sources.dosages
     )
   }
@@ -59,9 +63,11 @@ object Sources {
       mcoCePath = paths.mcoCe,
       hadPath = paths.had,
       ssrPaths = paths.ssr,
+      ssrCePath = paths.ssrCe,
       irBenPath = paths.irBen,
       irImbPath = paths.irImb,
       irPhaPath = paths.irPha,
+      irNatPath = paths.irNat,
       dosagesPath = paths.dosages
     )
   }
@@ -72,22 +78,25 @@ object Sources {
     mcoPath: Option[String] = None,
     mcoCePath: Option[String] = None,
     hadPath: Option[String] = None,
-    //@todo The merge of ssr_sej and ssr_c should be finally moved to the Flattening project
-    ssrPaths: Option[List[String]] = None,
+    ssrPaths: Option[String] = None,
+    ssrCePath: Option[String] = None,
     irBenPath: Option[String] = None,
     irImbPath: Option[String] = None,
     irPhaPath: Option[String] = None,
+    irNatPath: Option[String] = None,
     dosagesPath: Option[String] = None): Sources = {
 
     Sources(
       dcir = dcirPath.map(DcirSource.read(sqlContext, _)),
       mco = mcoPath.map(McoSource.read(sqlContext, _)),
       mcoCe = mcoCePath.map(McoCeSource.read(sqlContext, _)),
-      had = hadPath.map(HadSource.read(sqlContext, _)),
       ssr = ssrPaths.map(SsrSource.read(sqlContext, _)),
+      ssrCe = ssrCePath.map(SsrCeSource.read(sqlContext, _)),
+      had = hadPath.map(HadSource.read(sqlContext, _)),
       irBen = irBenPath.map(IrBenSource.read(sqlContext, _)),
       irImb = irImbPath.map(IrImbSource.read(sqlContext, _)),
       irPha = irPhaPath.map(IrPhaSource.read(sqlContext, _)),
+      irNat = irNatPath.map(IrNatSource.read(sqlContext, _)),
       dosages = dosagesPath.map(DosagesSource.read(sqlContext, _))
     )
   }
