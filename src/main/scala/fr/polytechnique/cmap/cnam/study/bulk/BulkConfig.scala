64a551399c44801a9e8baf2e6390a967a105baba
Dian SUN <sun.dian0720@gmail.com>
2020-02-25 16:20:48 +0100

    CNAM-442: exact date in medical act
    1. a fake date for null start date in dcir
    2. a day offset for start date in mco
    3. bulk config for drug level

--- src/main/scala/fr/polytechnique/cmap/cnam/study/bulk/BulkConfig.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/study/bulk/BulkConfig.scala
@@ -4,19 +4,20 @@ package fr.polytechnique.cmap.cnam.study.bulk
 
 import java.time.LocalDate
 import pureconfig.generic.auto._
+import fr.polytechnique.cmap.cnam.etl.config.BaseConfig
 import fr.polytechnique.cmap.cnam.etl.config.study.StudyConfig
-import fr.polytechnique.cmap.cnam.etl.config.{BaseConfig, ConfigLoader}
 import fr.polytechnique.cmap.cnam.etl.extractors.drugs.DrugConfig
-import fr.polytechnique.cmap.cnam.etl.extractors.drugs.level.Cip13Level
+import fr.polytechnique.cmap.cnam.etl.extractors.drugs.classification.DrugClassConfig
+import fr.polytechnique.cmap.cnam.etl.extractors.drugs.level.{Cip13Level, DrugClassificationLevel}
 
 case class BulkConfig(
   input: StudyConfig.InputPaths,
-  output: StudyConfig.OutputPaths) extends StudyConfig {
-  val drugs: DrugConfig = BulkConfig.DrugsConfig
+  output: StudyConfig.OutputPaths,
+  drugs: DrugConfig = BulkConfig.DrugsConfig()) extends StudyConfig {
   val base: BaseConfig = BulkConfig.BaseConfig
 }
 
-object BulkConfig extends ConfigLoader {
+object BulkConfig extends BulkConfigLoader {
 
   def load(path: String, env: String): BulkConfig = {
     val defaultPath = "config/bulk/default.conf"
@@ -29,9 +30,9 @@ object BulkConfig extends ConfigLoader {
     studyEnd = LocalDate.of(2015, 1, 1)
   )
 
-  final object DrugsConfig extends DrugConfig(
-    level = Cip13Level,
-    families = List.empty
-  )
+  final case class DrugsConfig(
+    override val level: DrugClassificationLevel = Cip13Level,
+    override val families: List[DrugClassConfig] = List.empty
+  ) extends DrugConfig(level = level, families = families)
 
 }
