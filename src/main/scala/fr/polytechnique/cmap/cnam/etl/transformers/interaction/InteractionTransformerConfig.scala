685862d653a54b3daf8bb57bdcb478f345aad385
Youcef Sebiat <youcefsebiat@gmail.com>
2020-04-09 14:27:58 +0200

    CNAM-457: MinimumDuration parameter for Interaction Transformer.

--- src/main/scala/fr/polytechnique/cmap/cnam/etl/transformers/interaction/InteractionTransformerConfig.scala
+++ src/main/scala/fr/polytechnique/cmap/cnam/etl/transformers/interaction/InteractionTransformerConfig.scala
@@ -2,10 +2,17 @@
 
 package fr.polytechnique.cmap.cnam.etl.transformers.interaction
 
+import me.danielpes.spark.datetime.{Period => Duration}
+import me.danielpes.spark.datetime.implicits._
 import fr.polytechnique.cmap.cnam.etl.transformers.TransformerConfig
 
-class InteractionTransformerConfig(val level: Int) extends TransformerConfig
+class InteractionTransformerConfig(val level: Int, val minimumDuration: Duration) extends TransformerConfig
 
 object InteractionTransformerConfig {
-  def apply(level: Int = 3): InteractionTransformerConfig = new InteractionTransformerConfig(level)
+  def apply(
+    level: Int = 3,
+    minimumDuration: Duration = 30.days): InteractionTransformerConfig = new InteractionTransformerConfig(
+    level,
+    minimumDuration
+  )
 }
